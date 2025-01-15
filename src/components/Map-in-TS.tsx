d272b3739959278a1f3cdcbf86cf75a17eb04688
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-01-15 20:48:50 +0900

    출력 변경

--- src/components/Map-in-TS.tsx
+++ src/components/Map-in-TS.tsx
@@ -2,10 +2,22 @@ import { useEffect, useState } from "react";
 import MakeMap from "../logic/makdMap";
 
 export default function MapInTS() {
-  const [item, setItem] = useState<Map<number, string>>();
+  const [item, setItem] = useState<Map<number, string>>(new Map());
   useEffect(() => {
     setItem(MakeMap());
   }, []);
 
-  return <>{item}</>;
+  return (
+    <>
+      <div>
+        {Array.from(item.entries()).map((value, key) => {
+          return (
+            <h1>
+              {key} = {value}
+            </h1>
+          );
+        })}
+      </div>
+    </>
+  );
 }
